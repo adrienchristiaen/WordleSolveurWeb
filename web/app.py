634020317b8677ad6c0078215c5d05ba1d79819f
Becquet Albert <albert.becquet@telecomnancy.eu>
2022-03-28 08:58:03 +0200

    Ajout fonctionnalités Inscription/Connexion/Déconnexion (toutes les fonctionnalités n'ont pas encore pu être testées)

--- web/app.py
+++ web/app.py
@@ -44,9 +44,95 @@ if __name__ == "__main__":
     app.run()
 
 #Accueil
+@app.route('/')
+@app.route('/accueil')
+def accueil():
+    return render_template("accueil.html")
 
 #Statistiques
 
 #Mode de jeu/Paramètres
 
-#Inscription/Connexion
+#Inscription/Connexion/Déconnexion ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#Inscription
+@app.route('/inscription', methods=['GET', 'POST'])
+def inscription():
+    if request.method == "POST":
+        #Informations du formulaire
+        username = request.form.get("username")
+        password = request.form.get("password")
+        email = request.form.get("email")
+        #Connexion base de données
+        con = sqlite3.connect('data.sql')
+        cur = con.cursor()
+        #Vérification de l'unicité de l'utilisateur
+        req = cur.execute("SELECT COUNT(username) FROM utilisateur WHERE username = (?)", ([username]))
+        count = req.fetchone()[0]
+        if count != 0:
+            flash("Ce nom d'utilisateur est déjà utilisé. Choisissez en un autre.")
+        else:
+            #Vérification de l'unicité de l'adresse email
+            req = cur.execute("SELECT COUNT(email) FROM utilisateur WHERE email = (?)", ([email]))
+            count = req.fetchone()[0]
+            if count != 0:
+                flash("Cet email est déjà utilisé. Choisissez en un autre.")
+            else:
+                #Comptage du nombre d'utilisateurs
+                req = cur.execute("SELECT COUNT(username) FROM utilisateur")
+                num = req.fetchone()[0] + 1
+                #Insertion de l'utilisateur dans la BD
+                cur.execute("INSERT INTO utilisateur(id, username, password, email) VALUES (?,?,?,?)", (num, username, password, email))
+                con.commit()
+                #Création du compte et connexion
+                session['username'] = username
+                user = User(id = num)
+                login_user(user)
+                flash("Votre nouveau compte a été créé avec succès !")
+                return redirect("accueil")
+
+    return render_template('inscription.html')
+
+#Connexion
+@app.route('/connexion', methods=['GET', 'POST'])
+def connexion():
+    if request.method == "POST":
+        #Informations du formulaire
+        username = request.form.get("username")
+        password = request.form.get("password")
+        #Connexion base de données
+        con = sqlite3.connect('data.sql')
+        cur = con.cursor()
+        #Vérification de la validité username + pwd
+        req = cur.execute("SELECT COUNT(username) FROM utilisateur WHERE username = (?)", ([username]))
+        count = req.fetchone()[0]
+        if count == 0:
+            flash("Ce nom d'utilisateur n'existe pas. Veuillez réessayer.")
+        else:
+            req = cur.execute("SELECT password FROM utilisateur WHERE username = (?)", ([username]))
+            pwd = req.fetchone()[0]
+            if password != pwd:
+                flash("Mauvais mot de passe. Veuillez réessayer.")
+            else:
+                #Récupération de l'identifiant associé à l'utilisateur
+                req = cur.execute("SELECT id FROM utilisateur WHERE username = (?)", ([username]))
+                num = req.fetchone()[0]
+                #Connexion de l'utilisateur
+                session['username'] = username
+                user = User(id = num)
+                login_user(user)
+                flash("Vous vous êtes connecté avec succès !")
+                return redirect("accueil")
+
+    return render_template('connexion.html')
+
+#Déconnexion
+@app.route("/deconnexion")
+@login_required
+def deconnexion():
+    session.pop('username', None)
+    logout_user()
+    flash("Vous vous êtes déconnecté avec succès. A la prochaine !")
+    return redirect("accueil")
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
