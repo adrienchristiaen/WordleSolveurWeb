5c1bbb4fae2af33c95e1348f63300f1f6d6dfb27
LACROIX Thomas <thomas.lacroix@telecomnancy.eu>
2022-04-11 21:47:54 +0200

    Mise sous forme de fonctions du code wordle

--- web/app.py
+++ web/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, request, url_for, flash, session, g
 from flask_login import LoginManager, UserMixin, login_required, logout_user, current_user, login_user
 import sqlite3
-from selection import *
+from fonctions_wordle_flask import *
 app = Flask(__name__)
 
 #Configuration
@@ -67,91 +67,33 @@ def accueil(nb_lettres=None, nb_essais=None,mode_de_jeu=None,mot_cherche=None, l
     cur = con.cursor()
 
     #________________Récupération depuis la base de données________________#
-    nb_essais=cur.execute("SELECT Nb_essais FROM Modes ")
-    nb_essais=nb_essais.fetchall()
-    #print("nb_essais :",nb_essais)
+    nb_essais = recup_table()[0]
 
-    nb_lettres=cur.execute("SELECT Nb_caracteres FROM Modes ")
-    nb_lettres=nb_lettres.fetchall()[0]
-    #print(nb_lettres)
+    nb_lettres = recup_table()[1]
+    nb_lettres = nb_lettres[0][0]                                          #Idem mot_cherche
 
-    mot_cherche=cur.execute("SELECT mot_cherche FROM Modes ")
-    mot_cherche=mot_cherche.fetchall()[0]
-    #print(mot_cherche)
+    mot_cherche = recup_table()[2]
+    mot_cherche = mot_cherche[0][0]                                        #On prend le premier élément de la liste mot_cherche (tous les éléments sont identiques)
 
-    mots_proposes=cur.execute("SELECT mots_proposes FROM Modes ")
-    mots_proposes=mots_proposes.fetchall()
-    #print(mots_proposes)
+    mots_proposes = recup_table()[3]
 
-    etat_lettres=cur.execute("SELECT etat_lettres FROM Modes ")
-    etat_lettres=etat_lettres.fetchall()
-    #print(etat_lettres)
+    etat_lettres = recup_table()[4]
 
-    mode_de_jeu=cur.execute("SELECT Mode_de_jeu FROM Modes ")
-    mode_de_jeu=mode_de_jeu.fetchall()[0][0]
-    #print(mode_de_jeu)
-
+    mode_de_jeu = recup_table()[5]
+    mode_de_jeu =  mode_de_jeu[0][0]
     #______________________________________________________________________#
-
-    #__________________Mise des tuples sous forme de liste_________________#
-    mot_cherche=mot_cherche[0]                                             #On prend le premier élément de la liste mot_cherche (tous les éléments sont identiques)
-    nb_lettres=nb_lettres[0]                                               #Idem mot_cherche
-
+    
     for i in range(len(nb_essais)):
         nb_essais[i]=nb_essais[i][0]                                       #On passe d'une liste de la forme [(5,)(6,)] à [5,6]
     nb_essais.reverse()                                                    #Car je souhaite une liste décroissant : [5,6] => [6,5]
     #print("nb_essais :",nb_essais)
 
-    liste_mot_propose=[]                                                   #Liste contenant tous les mots proposés par le joueur
-    for i in range(len(mots_proposes)):                                    #Exemple : ['Bondir','Rouges','......','......']
-        liste_mot_propose.append(mots_proposes[i][0])                      #On ajoute les mots déja proposés qui se trouvent dans la BD
-    #print('avant test',liste_mot_propose)
-    if liste_mot_propose[0]=='':                                           #On regarde si aucun mot n'a encore été proposé
-        remplissage = nb_essais[0]-len(liste_mot_propose)                  #Jusqu'au else, le code permet de générer liste_mot_propose de la forme suivante :
-        for j in range(remplissage):                                       #liste_mot_propose = [('.'*nb_lettres)*nb_essais]
-            liste_mot_propose.append('')
-        for k in range (len(liste_mot_propose)):
-            if liste_mot_propose[k]=='':
-                liste_mot_propose[k]=point*nb_lettres
-    else:
-        #print('else',liste_mot_propose)                                   #Si la liste contient déja un mot
-        liste_mot_propose.reverse()                                        #On l'inverse ['Bonjour','']  => ['','Bonjour']
-        liste_mot_propose.pop(0)                                           #On spprime l'élément 0 => ['Bonjour']
-        #print(liste_mot_propose)
-        remplissage = nb_essais[0]-len(liste_mot_propose)                  #On remplit selon la même méthode au-dessus
-        for i in range(remplissage):
-            liste_mot_propose.append('')
-        for k in range (len(liste_mot_propose)):
-            if liste_mot_propose[k]=='':
-                liste_mot_propose[k]=point*nb_lettres
-    print(liste_mot_propose)
+    #__________________Mise des tuples sous forme de liste_________________#
+    liste_mot_propose = creation_liste_mots_proposes(nb_lettres,nb_essais,mots_proposes,point)
+    print('liste_mot_propose :',liste_mot_propose)
 
-
-    liste_etat_lettres=[]                                                  #Liste contenant l'état des lettres de tous les mots proposés
-    for i in range(len(etat_lettres)):                                     #Exemple : ['221001','201102','000000','000000']
-        liste_etat_lettres.append(etat_lettres[i][0])                      #On ajoute les etats de lettre déja proposés qui se trouvent dans la BD
-    #print('avant test',liste_etat_lettres)
-    if liste_etat_lettres[0]=='':
-        remplissage = nb_essais[0]-len(liste_etat_lettres)                 #Même méthode que pour liste_mots_proposes
-        for j in range(remplissage):
-            liste_etat_lettres.append('')
-        for k in range (len(liste_etat_lettres)):               
-            if liste_etat_lettres[k]=='':
-                liste_etat_lettres[k]=zero*nb_lettres                      #liste_etat_lettres = [('0'*nb_lettres)*nb_essais]
-        #print('if',liste_etat_lettres)
-    else:
-        liste_etat_lettres.reverse()
-        liste_etat_lettres.pop(0)
-        #print(liste_mot_propose)
-        remplissage = nb_essais[0]-len(liste_etat_lettres)
-        for i in range(remplissage):
-            liste_etat_lettres.append('')
-        for k in range (len(liste_etat_lettres)):
-            if liste_etat_lettres[k]=='':
-                liste_etat_lettres[k]=zero*nb_lettres
-    #print('else',liste_etat_lettres)
-
-
+    liste_etat_lettres = creation_liste_etat_lettres(nb_lettres,nb_essais,etat_lettres,zero)
+    print('liste_etat_lettres :',liste_etat_lettres)
     #______________________________________________________________________#
 
     #___________________________Si la partie commmence_____________________#
@@ -166,16 +108,7 @@ def accueil(nb_lettres=None, nb_essais=None,mode_de_jeu=None,mot_cherche=None, l
     #______________________________________________________________________#
     
     if request.method == "POST":
-        #_______________________Place la première lettre_______________________#
-        test=True
-        #print(test,liste_mot_propose)
-        for i in range(len(liste_mot_propose)):
-            if liste_mot_propose[i] == point*nb_lettres and test:                   #Place la première lettre uniquement pour le prochain mot :
-                liste_mot_propose[i]=mot_cherche[0]+point*(nb_lettres-1)            #mot_cherche = 'PYTHON'
-                test=False                                                          #liste_mot_propose = ['ROULER','P.....','......']                                          
-                #print('ca passe')  
-        #print(liste_mot_propose)
-        #______________________________________________________________________#
+        liste_mot_propose = place_premiere_lettre(nb_lettres,liste_mot_propose,mot_cherche,point)       #On place la première lettre dans le mot a deviné
         if nb_essais[-1]>0:
             print("Le mot à trouver est : ",mot_cherche)
             mot_propose=request.form.get("mot_propose")
@@ -188,26 +121,7 @@ def accueil(nb_lettres=None, nb_essais=None,mode_de_jeu=None,mot_cherche=None, l
 
                 print("Le mot proposé est : ",mot_propose)
 
-                etat_lettres=''
-                liste_de_test =[]
-                for l in range(len(mot_propose)):           #0 : la lettre ne fait partie du mot proposé
-                    liste_de_test.append([mot_propose[l],0]) #1 : la lettre proposé à cet emplacement fait partie du mot mais n'est pas au bon endroit (ici ['u',1] si on a proposé toi mais pas pour uti)
-                #print(liste_de_test)
-
-                for i in range(len(mot_propose)):
-                    if liste_de_test[i][1]==1:           #On initialise l'état des lettres à 1 pour le mettre à 0 sinon certaine les - vont rester même s'il n'y a pas de lettre mal placée
-                        liste_de_test[i][1]=0
-
-                    if mot_propose[i]==mot_cherche[i]:          #Si la lettre proposé dans le mot est au bon emplacement son état passe à 2
-                        liste_de_test[i][1]=2
-
-                    else:
-                        for j in range(len(mot_cherche)):                           #On va regarder si parmis les lettres proposés, certaines sont au mauvais endroit
-                            if mot_propose[i]==mot_cherche[j] and liste_de_test[j][1]!=2:    
-                                liste_de_test[i][1]=1
-                for i in range(len(liste_de_test)):
-                    etat_lettres+=str(liste_de_test[i][1])
-                #print(etat_lettres)
+                etat_lettres = calcul_etat_lettres(mot_cherche, mot_propose)        #Calcul de l'état des lettres
                 
                 liste_etat_lettres[nb_essais[0]-nb_essais[-1]] = etat_lettres       #On ajoute l'état des lettres du mot dans la liste liste_etat_lettres
     
@@ -219,16 +133,7 @@ def accueil(nb_lettres=None, nb_essais=None,mode_de_jeu=None,mot_cherche=None, l
                 if etat_lettres == '2'*nb_lettres:                                  #Si le dernier etat_lettres = '2222222222' par exemple c'est que le mot est trouvé
                     return render_template("accueil_win.html",nb_lettres=nb_lettres, nb_essais=nb_essais,mode_de_jeu=mode_de_jeu,mot_cherche=mot_cherche, liste_mot_propose=liste_mot_propose,liste_etat_lettres=liste_etat_lettres)
                 else:
-                    #_______________________Place la première lettre_______________________#
-                    test=True
-                    #print(test,liste_mot_propose)
-                    for i in range(len(liste_mot_propose)):
-                        if liste_mot_propose[i] == point*nb_lettres and test:                   #Code déja expliqué
-                            liste_mot_propose[i]=mot_cherche[0]+point*(nb_lettres-1)
-                            test=False
-                            #print('ca passe')
-                    #print(liste_mot_propose)
-                    #______________________________________________________________________#
+                    liste_mot_propose = place_premiere_lettre(nb_lettres,liste_mot_propose,mot_cherche,point) #On place la première lettre dans le mot a deviné
                     return render_template("accueil.html",nb_lettres=nb_lettres, nb_essais=nb_essais,mode_de_jeu=mode_de_jeu,mot_cherche=mot_cherche, liste_mot_propose=liste_mot_propose,liste_etat_lettres=liste_etat_lettres)
             else:
                 #print("mot non valide")
@@ -238,16 +143,7 @@ def accueil(nb_lettres=None, nb_essais=None,mode_de_jeu=None,mot_cherche=None, l
                 return render_template("accueil_lose.html",nb_lettres=nb_lettres, nb_essais=nb_essais,mode_de_jeu=mode_de_jeu,mot_cherche=mot_cherche, liste_mot_propose=liste_mot_propose,liste_etat_lettres=liste_etat_lettres)
     else:
         print("Le mot à trouver est : ",mot_cherche)
-        #_______________________Place la première lettre_______________________#
-        test=True
-        #print(test,liste_mot_propose)
-        for i in range(len(liste_mot_propose)):
-            if liste_mot_propose[i] == point*nb_lettres and test:
-                liste_mot_propose[i]=mot_cherche[0]+point*(nb_lettres-1)
-                test=False
-                #print('ca passe')
-        #print(liste_mot_propose)
-        #______________________________________________________________________#
+        liste_mot_propose = place_premiere_lettre(nb_lettres,liste_mot_propose,mot_cherche,point) #On place la première lettre dans le mot a deviné
         return render_template("accueil.html",nb_lettres=nb_lettres, nb_essais=nb_essais,mode_de_jeu=mode_de_jeu,mot_cherche=mot_cherche, liste_mot_propose=liste_mot_propose,liste_etat_lettres=liste_etat_lettres)
 
 
@@ -265,7 +161,7 @@ def rejouer():
     # nb_lettres=nb_lettres.fetchall()[0][0]
     # #print(nb_lettres)
 
-    # mode_de_jeu=cur.execute("SELECT Mode_de_jeu FROM Modes ")                         #Code déja expliqué
+    # mode_de_jeu=cur.execute("SELECT Mode_de_jeu FROM Modes ")                         #Code a développer
     # mode_de_jeu=mode_de_jeu.fetchall()[0][0]
     # #print(mode_de_jeu)
 
