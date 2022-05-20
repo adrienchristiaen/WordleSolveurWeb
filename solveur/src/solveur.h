2695e38cdea5041a5edf0e747a57cca4eaa38099
LACROIX Thomas <thomas.lacroix@telecomnancy.eu>
2022-05-20 21:45:45 +0200

    Développement en cours de la liste chainée

--- solveur/src/solveur.h
+++ solveur/src/solveur.h
@@ -1,6 +1,10 @@
 #ifndef __SOLVEUR_H__
 #define __SOLVEUR_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 
 
 typedef struct element_t element_t;
@@ -23,6 +27,16 @@ struct list_t
 
 
 /*_______________A_COMPLETER__________________*/
+list_t *list_create();
 
+void ajout_mots(list_t* list_vide);
+
+double freq_letters(list_t *liste_mots, char lettre);
+
+void list_destroy(list_t *liste_mots);
+
+void list_print(list_t *liste_mots);
+
+void list_append(list_t *one_list, char *one_key, double one_value);
 
 #endif /*__SOLVEUR_H__*/
---
+++ solveur/src/solveur_test.c
@@ -0,0 +1,12 @@
+#include "solveur.h"
+
+int main()
+{
+    list_t* liste_mots = list_create();
+    //ajout_mots(liste_mots);
+    //list_print(liste_mots);
+    list_destroy(liste_mots);
+    return 0;
+}
+
+
