f8a7dee9ba984e21f396532172280492dec9c183
Adrien Christiaen <adrien.christiaen@telecomnancy.eu>
2022-05-26 08:57:44 +0200

    modification nom fonction en c

---
+++ solveur/src/delword.c
@@ -0,0 +1,31 @@
+#include "suppr.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void suppr_tout_critere(t_elem**prem,int val)
+{
+t_elem*n,*sup,*prec;
+    // supprimer au début
+    while(*prem!=NULL && (*prem)->valeur==val){
+        sup=*prem;
+        *prem=(*prem)->suivant;
+        free(sup);
+    }
+    // les suivants
+    if (*prem!=NULL){
+        prec=*prem;
+        n=prec->suivant;
+        while (n!=NULL){
+            while(n!=NULL && n->valeur==val){
+                sup=n;
+                n=n->suivant;
+                prec->suivant=n;
+                free(sup);
+            }
+            if (n!=NULL){
+                prec=n;
+                n=n->suivant;
+            }
+        }
+    }
+}
