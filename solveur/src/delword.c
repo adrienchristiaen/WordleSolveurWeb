048fbd11ca5dee97ca786070ba25924e54725c90
Adrien Christiaen <adrien.christiaen@telecomnancy.eu>
2022-05-26 19:21:55 +0200

    Modification totale fonction delete word en c

--- solveur/src/delword.c
+++ solveur/src/delword.c
@@ -1,31 +1,49 @@
-#include "delword.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "solveur.h"
 
-void suppr_tout_critere(t_elem**prem,int val)
+
+void suppr(struct list_t *oneList, struct element_t *elem)
 {
-t_elem*n,*sup,*prec;
-    // supprimer au début
-    while(*prem!=NULL && (*prem)->valeur==val){
-        sup=*prem;
-        *prem=(*prem)->suivant;
-        free(sup);
+    struct element_t *cur;
+    struct element_t *prev;
+ 
+    // Si la liste est vide il n'y a rien à effacer
+    if (oneList->premier == NULL) return;
+ 
+    // Positionnement des deux éléments "prev" et "cur" sur l'élément juste avant et sur l'élément à effacer
+    for (cur=oneList->premier, prev=NULL; cur != elem; prev=cur, cur=cur->suivant);
+ 
+    // Si l'élément précédent existe
+    if (prev)
+        // L'élément précédent prend l'adresse du suivant
+        prev->suivant=cur->suivant;
+    else
+        // L'élément à supprimer était le premier => celui-ci change
+        oneList->premier=cur->suivant;
+ 
+    // Si l'élément à effacer était le dernier
+    if (cur->suivant == NULL)
+        // Le dernier élément de la liste change
+        oneList->dernier=cur;
+ 
+    // L'élément à effacer est supprimé
+   free(elem);
+}
+
+
+// Fonction qui efface un mot de la liste
+void deleteWord(char *word, struct list_t *oneList)
+{
+    struct element_t *cur;
+ 
+    // Recherche du mot dans la liste
+    for (cur=oneList->premier; cur != NULL; cur=cur->suivant)
+    {
+          if (strcmp(cur->mot, word) == 0)
+             break;
     }
-    // les suivants
-    if (*prem!=NULL){
-        prec=*prem;
-        n=prec->suivant;
-        while (n!=NULL){
-            while(n!=NULL && n->valeur==val){
-                sup=n;
-                n=n->suivant;
-                prec->suivant=n;
-                free(sup);
-            }
-            if (n!=NULL){
-                prec=n;
-                n=n->suivant;
-            }
-        }
-    }
+ 
+    // Si l'élément contenant le mot a été trouvé
+    if (cur)
+        // Il est supprimé de la liste
+       suppr(oneList, cur);
 }
